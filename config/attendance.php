bc93be26af670a043294bd300a023b6985576868
Sayyad Ali Haider Sherazi <mrhyder29@gmail.com>
2026-07-21 14:19:10 +0500

    changed the configuration

--- config/attendance.php
+++ config/attendance.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'office_latitude' => (float) env('OFFICE_LATITUDE', 31.4504),
-    'office_longitude' => (float) env('OFFICE_LONGITUDE', 73.1350),
+    'office_latitude' => (float) env('OFFICE_LATITUDE', 31.411751),
+    'office_longitude' => (float) env('OFFICE_LONGITUDE', 73.117245),
     'max_checkin_distance_meters' => (int) env('MAX_CHECKIN_DISTANCE_METERS', 100),
 ];
